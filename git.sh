3ac0841629e3a47649686e1346c1aeeb93569f9a
varunpnair92 <varunpnair92@gmail.com>
2026-08-03 05:39:29 +0000

    Added git conf

---
+++ git.sh
@@ -0,0 +1,6 @@
+git@github.com:varunpnair92/timetable
+https://github.com/flutter/flutter.git
+git@github.com:varunpnair92/apiapp
+git@github.com:varunpnair92/labutilisation
+https://github.com/flutter/flutter.git
+git@github.com:varunpnair92/scripts
