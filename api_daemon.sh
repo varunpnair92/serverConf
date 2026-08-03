9c78b9e55ff6a3de9fe07f11cca12d0bc3f76f81
varunpnair92 <varunpnair92@gmail.com>
2026-08-03 05:25:24 +0000

    Added daemon ralod

---
+++ api_daemon.sh
@@ -0,0 +1,5 @@
+sudo systemctl daemon-reload
+sudo systemctl restart gunicornapi
+sudo systemctl restart gunicornvillagecafe
+sudo systemctl restart gunicorntimetableweb
+sudo systemctl restart gunicorntimet
